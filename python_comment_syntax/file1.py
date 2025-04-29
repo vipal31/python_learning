54cc24b5e2c6d3047372edcd7cdc9c6628475b0c
Vipal Gujrathi <vipal.gujrathi@hotmail.com>
2025-04-29 17:55:22 +0530

    initial commit msg

---
+++ python_comment_syntax/file1.py
@@ -0,0 +1,22 @@
+''' This is first exercise in my journey to learn python
+to stay relevant in the industry'''
+
+
+# This line will print my favorite code, so that user can read it
+
+print("Change is the only constant thing in the world!, be curious")
+
+# this line will print my name and age
+
+name = 'Vipal'
+age = 35
+
+print(f"My name is {name} and I am {age} years old")
+
+## Below code will print if X is higher than 5 or not
+
+x=10
+if x>5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
---
+++ python_comment_syntax/file2.py
@@ -0,0 +1,30 @@
+'''This file contain basic questions to understand command syntax and code readability'''
+
+## this command will print addition, subtraction of two given numbers
+
+number_1 = 10
+number_2 = 20
+
+## this command will print addition of two numbers
+
+print(f"Addition: {number_1 + number_2}")
+
+## this command will print subtraction of two numbers
+
+print(f"Subtraction: {number_2 - number_1}")
+
+
+# User is asked to enter city and print output
+
+city_name = input("Enter your city name: ")
+print(f"You live in {city_name}")
+
+
+## below command will print if given number is even or not
+
+y=20
+    
+if y%2==0:
+    print("y is even")
+else:
+    print("y is odd")
